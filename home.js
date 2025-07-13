4687930ccdbef5d0f66d0c50893a508c2d276fbb
surya <sbsurya159@gmail.com>
2025-07-13 20:55:50 +0530

    update

--- home.js
+++ home.js
@@ -1,23 +1,39 @@
-    const carousel = document.getElementById('carousel');
-    const itemWidth = carousel.offsetWidth;
-    
-    function scrollCarousel(direction) {
-        carousel.scrollLeft += direction * itemWidth;
-    }
-    
-    
-    
-    // data fetch for stories
-    
-    fetch("log.json").then((get)=> get.json()).then((got)=>{
-        got.stories.map((i)=>{
-            
-            carousel.innerHTML += `
-            <div class="carousel-div">
-                <div class="carousel-item-info" style="background-image: url('${i.img}')"></div>
-                <p class="carousel-user" style="text-align: center;">${i.name}</p>
-                </div>`;
-                
-            })
-        })
+const carousel = document.getElementById("carousel");
+const itemWidth = carousel.offsetWidth;
+const mprofile = document.getElementById("mprofilesec");
+
+function scrollCarousel(direction) {
+  carousel.scrollLeft += direction * itemWidth;
+}
+
+// data fetch for stories
+
+fetch("log.json")
+  .then((get) => get.json())
+  .then((got) => {
+    got.stories.map((i) => {
+      carousel.innerHTML += `
+           <button>
+                <div class="carousel-div">
+                    <div class="carousel-item-info" style="background-image: url('${i.img}')"></div>
+                    <p class="carousel-user" style="text-align: center;">${i.name}</p>
+                </div> 
+            </button>`;
+ 
+            if( `${i.id}` < 5){
+
+                mprofile.innerHTML += `
+                      <button>
+                          <div class="profile-div">
+                            <div class="profile-info"></div>
+                            <p class="puser-name">${i.name}</p>
+                            <hr>
+                          </div>
+                      </button>
+                      `;
+            }
+
         
+            
+    });
+  });
